5648b3e7055460b8cda83579450315a8e7a56806
uwu <hello@shadyx.com>
2017-08-20 00:48:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -400,7 +400,7 @@ extern "C" void* ThreadStats(void*) {
   return nullptr;
 }
 
-static const string mainnet_seeds[] = {"dns.weebcoin.com", ""};
+static const string mainnet_seeds[] = {"dnsseed.weebcoin.com", ""};
 static const string testnet_seeds[] = {"testnet-seed.litecointools.com", "seed-b.litecoin.loshan.co.uk", "testnet-seed.ltc.xurious.com", "dnsseed-testnet.thrasher.io", ""};
 static const string *seeds = mainnet_seeds;
 
