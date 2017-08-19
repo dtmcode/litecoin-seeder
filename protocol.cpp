adfba14997e7ce2de3f91453f17b4dd3e1a6da58
uwu <hello@shadyx.com>
2017-08-20 00:29:55 +0100

    Weebcoin changes

--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0xfb, 0xc0, 0xb6, 0xdb };
+unsigned char pchMessageStart[4] = { 0xc3, 0xd2, 0xd1, 0xbd };
 
 CMessageHeader::CMessageHeader()
 {
